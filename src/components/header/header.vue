7f325bd5988e3949fd7d67b2b813311e16210830
Si Chen <970831768@qq.com>
2017-07-10 23:36:14 +0800

    header组建完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,17 +30,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li v-for="(item, index) in seller.supports" class="support-item" :key="index">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -64,6 +87,9 @@ export default{
   methods: {
     showDetail: function() {
       this.detailShow = true;
+    },
+    hideDetail: function() {
+      this.detailShow = false;
     }
   },
   components: {
@@ -173,7 +199,6 @@ export default{
         margin-left: 2px;
         line-height: 24px;
       }
-      
     }
   }
 
@@ -226,8 +251,17 @@ export default{
     z-index: 100;        
     width: 100%;
     height: 100%;
-    background: rgba(7, 17, 27, 0.8);
     overflow: auto;
+    opacity: 1;
+    background: rgba(7, 17, 27, 0.8);
+    &.fade-enter-active, &.fade-leave-active{
+      transition: all 0.5s;
+    }
+    &.fade-enter, &.fade-leave-active {
+      opacity: 0;
+      background: rgba(7, 17, 27, 0);
+    }
+    backdrop-filter: blur(10px);// 只有ios才可以实现
 
     .detail-wrapper {
       width: 100%;
@@ -242,6 +276,83 @@ export default{
           font-size: 16px;
           line-height: 16px;
           text-align: center;
+        }
+
+        .star-wrapper {
+          margin-top: 18px;
+          padding: 2px 0;
+          text-align: center;
+        }
+
+        .title {
+          display: flex;
+          width: 80%;
+          margin: 28px auto 24px auto;
+
+          .line {
+            flex: 1;
+            position: relative; 
+            top: -6px;
+            border-bottom: 1px solid rgba(255, 255,255, 0.2);
+          }
+          .text {
+            padding: 0 12px;
+            font-size: 14px;
+            font-weight: 700;
+          }
+        }
+
+        .supports {
+          width: 80%;
+          margin: 0 auto;
+
+          .support-item {
+            margin-bottom: 12px;            
+            padding: 0 12px;
+            font-size: 0;
+            &:last-child {
+              margin-bottom: 0;
+            }
+
+            .icon {
+              display: inline-block;
+              vertical-align: top;              
+              width: 16px;
+              height: 16px;
+              margin-right: 6px;
+
+              &.decrease {
+                @include bg-image('./decrease_2');
+              }
+              &.discount {
+                @include bg-image('./discount_2');
+              }
+              &.guarantee {
+                @include bg-image('./guarantee_2');
+              }
+              &.invoice {
+                @include bg-image('./invoice_2');
+              }
+              &.special {
+                @include bg-image('./special_2');
+              }
+            }
+            .text {
+              font-size: 12px;
+              line-height: 16px;
+            }
+          }
+        }
+
+        .bulletin {
+          width: 80%;
+          margin: 0 auto;
+
+          .content {
+            padding: 0 12px;
+            line-height: 24px;
+            font-size: 12px;
+          }
         }
       }
     }
